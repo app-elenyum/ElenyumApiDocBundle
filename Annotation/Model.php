4f953dacc8f9c4ca5b9cb546b2882fafe91e01d0
bootta <bootta@yandex.ru>
2024-06-01 16:23:33 +0300

    added show modules

--- Annotation/Model.php
+++ Annotation/Model.php
@@ -19,7 +19,7 @@ use OpenApi\Generator;
  * @Annotation
  */
 #[\Attribute(\Attribute::TARGET_METHOD)]
-final class Model extends Attachable
+class Model extends Attachable
 {
     /** {@inheritdoc} */
     public static $_types = [
